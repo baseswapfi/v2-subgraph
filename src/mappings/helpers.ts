3f3497356d9ca77e9c1d915e28518faa311b8a8d
0xshoegazer <0xshoegazer@gmail.com>
2024-10-15 12:38:39 -0400

    init deploy

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -8,7 +8,7 @@ import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 import { TokenDefinition } from './tokenDefinition'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x22505cb4d5d10b2c848a9d75c57ea72a66066d8c'
+export const FACTORY_ADDRESS = '0x82995f682dc38b17b99079cf63df8d263c6d5ee0'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
